e9c94ef38ba3ebad992564e3b5e2591f1c2a3054
Emma-Ssali <ssaliemmanuel2000@gmail.com>
2026-04-08 15:43:58 +0300

    Student Dashboard setup

--- clearance/views.py
+++ clearance/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import ClearanceRequest, ClearanceApproval
+
 
 # Create your views here.
+@login_required
+def student_dashboard(request):
+    student = request.user.student
+
+    requests = ClearanceRequest.objects.filter(student=student)
+    approvals = ClearanceApproval.objects.filter(request__student=student)
+
+    return render(request, 'clearance/student_dashboard.html', {
+        'requests': requests,
+        'approvals': approvals,
+    })
--- university_clearance/settings.py
+++ university_clearance/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'university_clearance.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
